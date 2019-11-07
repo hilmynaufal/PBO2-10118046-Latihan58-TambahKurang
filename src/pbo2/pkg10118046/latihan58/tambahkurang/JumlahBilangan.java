5510f439f7a3368a501267972e46733c8a7b60dd
hilmynaufal <55622577+hilmynaufal@users.noreply.github.com>
2019-11-07 20:35:36 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118046/latihan58/tambahkurang/JumlahBilangan.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118046.latihan58.tambahkurang;
+
+/**
+ *
+ * @author ASUS
+ */
+public class JumlahBilangan extends Bilangan{
+    public void tampilHasilJumlah() {
+        System.out.println("Hasil Perjumlahan = " + (getX()+getY()));
+    }
+}
---
+++ src/pbo2/pkg10118046/latihan58/tambahkurang/PBO210118046Latihan58TambahKurang.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pbo2.pkg10118046.latihan58.tambahkurang;
+
+/**
+ *
+ * @author
+ *  Nama              : M. Hilmy Naufal
+ *  Kelas             : IF2
+ *  NIM               : 10118046
+ *  Deskripsi Program : menampilkan jumlah dan selisih bilangan
+ */
+public class PBO210118046Latihan58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JumlahBilangan jumlahBilangan = new JumlahBilangan();
+        SelisihBilangan selisihBilangan = new SelisihBilangan();
+        
+        jumlahBilangan.tampilHasilJumlah();
+        selisihBilangan.tampilSelisih();
+        
+    }
+
+}
